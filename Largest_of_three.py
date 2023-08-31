ef280b5b7f87559ecebc7d144f53061d3d471e41
Aarushi-iitm <143726192+Aarushi-iitm@users.noreply.github.com>
2023-08-31 20:15:07 +0530

    Update Largest_of_three.py

--- Largest_of_three.py
+++ Largest_of_three.py
@@ -1,17 +1,15 @@
+# Function to find the largest value of the three numbers
+
 import streamlit as st
 
-def find_largest(num1, num2, num3):
-    return max(num1, num2, num3)
-
 def main():
-    st.title("Find the Largest Number")
+    st.title("Find the Largest Value of the Three Numbers")
     
-    num1 = st.number_input("Enter the first number:")
-    num2 = st.number_input("Enter the second number:")
-    num3 = st.number_input("Enter the third number:")
+    n1 = st.number_input("Enter the first number:")
+    n2 = st.number_input("Enter the second number:")
+    n3 = st.number_input("Enter the third number:")
     
-    if st.button("Find Largest"):
-        largest = find_largest(num1, num2, num3)
-        st.write(f"The largest number is: {largest}")
-
+    if st.button("Display Largest"):
+        largest = max(n1, n2, n3)
+        st.write(f"The Largest Value is: {largest}")
 main()
